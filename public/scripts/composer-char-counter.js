8aa2ee1e8fcc280ee0d260707e33847e79717b93
Amy <amymccarthy2014@gmail.com>
2021-12-15 00:19:47 +0000

    remove inline style for negative counter; addClass and removeClass to change counter to red for negative numbers

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -7,8 +7,10 @@ $(document).ready(function() {
     document.getElementById("new-tweet-count").innerHTML = countDown;
 
     if (countDown < 0) {
-      document.getElementById("new-tweet-count").style.color = "red";
-    };
+      $("#new-tweet-count").addClass("new-tweet-error");
+    } else {
+      $("#new-tweet-count").removeClass("new-tweet-error");
+    }
   });
 
 });
